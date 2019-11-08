75ab625d22e42cc46b19618808deecff37e69a6a
Zhōu Bówēi 周伯威 <github@bowei.me>
2019-11-08 17:37:43 +0900

    :beer: pass name/description/imagelink variables from backend to template

--- server/handlers.go
+++ server/handlers.go
@@ -16,7 +16,10 @@ func getAllAuctions(c *gin.Context) {
 
 func getAuctionsById(c *gin.Context) {
 	c.HTML(http.StatusOK, "auction/index.tmpl", gin.H{
-		"title": "Test title",
+		"page_title":  "Test title",
+		"name":        "マウジー　ニット",
+		"description": "色はくすみピンク。丈が少し短めです。首元まであるのであったかいと思います。フリーサイズで新品未使用です。タグ付きです。元値6980円＋税。写真より薄めのピンクで普段使いしやすいです",
+		"image_link":  "https://static.mercdn.net/item/detail/orig/photos/m67918189515_1.jpg?1573189516",
 	})
 }
 
