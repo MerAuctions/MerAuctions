e9f8d9fe33ab88126a5cbd6f0bd8cc9817254fa3
Zhōu Bówēi 周伯威 <github@bowei.me>
2019-11-09 17:05:36 +0900

    :arrow_right: change type name of list of bids

--- models/models.go
+++ models/models.go
@@ -1,7 +1,7 @@
 package models
 
 import (
-  "github.com/golang/protobuf/ptypes/timestamp"
+	"github.com/golang/protobuf/ptypes/timestamp"
 )
 
 //Price of the item or the bid made on the item
@@ -11,36 +11,36 @@ type Price float32
 type ID string
 
 //User represents the users making the bid
-type User struct{
-  UserID ID
-  UserName string
+type User struct {
+	UserID   ID
+	UserName string
 }
 
 //Bid represents a single bid in an Auction
-type Bid struct{
-  BidID ID
-  AuctionID ID
-  UserID ID
-  Price Price
-  Time timestamp.Timestamp
+type Bid struct {
+	BidID     ID
+	AuctionID ID
+	UserID    ID
+	Price     Price
+	Time      timestamp.Timestamp
 }
 
 //Auction represents a single auction
-type Auction struct{
-  AuctionID ID
-  Image string      // image encode in base64
-  Description string
-  EndTime timestamp.Timestamp
+type Auction struct {
+	AuctionID   ID
+	Image       string // image encode in base64
+	Description string
+	EndTime     timestamp.Timestamp
 }
 
 //AuctionList is a list of auctions
 type AuctionList []Auction
 
-type Result struct{
-  AuctionID ID
-  WinnerID ID
-  Price Price
+type Result struct {
+	AuctionID ID
+	WinnerID  ID
+	Price     Price
 }
 
-//List of most recent bids
-type LatestBids []Bid
+//List of bids
+type BidList []Bid
