468016b8a494ceeb44e7efaea683497f16633aea
sipian <harshaga97@gmail.com>
2019-11-09 23:48:12 +0900

    read dbname from flag

--- cmd/auctions/merauctions.go
+++ cmd/auctions/merauctions.go
@@ -9,8 +9,9 @@ import (
 func main() {
 
 	dbURL := flag.String("mongodb-url", "mongodb://localhost:27017", "URL to connet to mongodb database")
+	dbName := flag.String("database", "testing", "Database name in mongodb")
 	flag.Parse()
 	router := server.CreateRouter()
-	server.ConnectToDB(*dbURL)
+	server.ConnectToDB(*dbURL, *dbName)
 	server.StartServer(router)
 }
