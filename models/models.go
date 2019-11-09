1a0e140019d41eafcc1e1626a87cd5f8e853cee8
Zhōu Bówēi 周伯威 <github@bowei.me>
2019-11-09 19:07:08 +0900

    :arrow_right: modify data structure

--- models/models.go
+++ models/models.go
@@ -1,14 +1,13 @@
 package models
 
-import (
-	"github.com/golang/protobuf/ptypes/timestamp"
-)
-
 //Price of the item or the bid made on the item
 type Price float32
 
 //ID represents the id of an auciton, user or a bid
 type ID string
+
+// A unix timestamp in milliseconds, like 1573292410953
+type TimeStamp int64
 
 //User represents the users making the bid
 type User struct {
@@ -22,15 +21,16 @@ type Bid struct {
 	AuctionID ID
 	UserID    ID
 	Price     Price
-	Time      timestamp.Timestamp
+	Time      TimeStamp
 }
 
 //Auction represents a single auction
 type Auction struct {
 	AuctionID   ID
+	Title       string
 	Image       string // image encode in base64
 	Description string
-	EndTime     timestamp.Timestamp
+	EndTime     TimeStamp
 }
 
 //AuctionList is a list of auctions
